f5bf744c8d12d77f33a1b9cb248fb7faf67f0d1a
Morshed <morshedalamustc22@gmail.com>
2019-11-17 00:31:37 +0600

    jwt added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const  jwt  = require('jsonwebtoken');
 const User = mongoose.model('User');
 const router = express.Router();
 
 router.post('/signup', async (req, res)=>{
 
    const {email, password} = req.body;
-   const user = new User({email, password});
-   await user.save();
+   try {
+       const user = new User({email, password});
+       await user.save();
+       const token = jwt.sign({ userId : user._id}, 'MY_SECRET_KEY');
+       res.send({token:token});
+   }catch (err) {
+       return  res.status(422).send(err.message);
+   }
 
-    res.send('you made a post request');
 });
 module.exports = router;
 
